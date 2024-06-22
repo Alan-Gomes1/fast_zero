420c5130ed60cf37fe513c29c65cff16ced9ece0
Alan <alan.gomes.ag28@gmail.com>
2024-06-21 23:46:47 -0300

    Add view hello

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,22 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from .schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello world'}
+
+
+@app.get('/hello', response_class=HTMLResponse)
+def hello():
+    return """
+        <html><body>
+            <h2>Hello World</h2>
+        <body><html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -5,7 +5,14 @@ from fastapi.testclient import TestClient
 from fast_zero.app import app
 
 
-def test_read_root_shold_return_200_ok():
+def test_read_root_returns_200_ok():
     client = TestClient(app)
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Hello world'}
+
+
+def test_content_page_hello():
+    client = TestClient(app)
+    response = client.get('/hello')
+    assert 'Hello World' in response.text
