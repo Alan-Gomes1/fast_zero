96280402d897d9c2cd64988914ada81620b82fd4
Alan <alan.gomes.ag28@gmail.com>
2024-07-23 21:32:10 -0300

    setting up session

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,12 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 
+from .database import get_session
+from .models import User
 from .schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
@@ -24,15 +28,43 @@ def hello():
 
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema):
-    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
-    database.append(user_with_id)
-    return user_with_id
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
+    db_user = session.scalar(
+        select(User).where(
+            (User.username == user.username) | (User.email == user.email)
+        )
+    )
+
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Username already exists'
+            )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Email already exists'
+            )
+
+    db_user = User(
+        username=user.username, password=user.password, email=user.email
+    )
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 
 @app.get('/users/', response_model=UserList)
-def read_users():
-    return {'users': database}
+def read_users(
+        limit: int = 10, skip: int = 0, session: Session = Depends(get_session)
+):
+    users = session.scalars(
+        select(User).limit(limit).offset(skip)
+    )
+    return {'users': users}
 
 
 @app.get('/users/{user_id}', response_model=UserPublic)
