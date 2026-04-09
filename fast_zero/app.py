5f04bae9ffe9256855a10ca51b0151d8b5328b35
Alan Gomes <alan.gomes.ag28@gmail.com>
2026-04-09 08:29:53 -0300

    feat: Adds todos router to API

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 
-from .routers import auth, users
+from .routers import auth, todos, users
 from .schemas import Message
 
 app = FastAPI()
 app.include_router(auth.router)
+app.include_router(todos.router)
 app.include_router(users.router)
 
 
