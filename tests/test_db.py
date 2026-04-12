a4af92a03b02b7ed0208edb8f59fe6c4ec121039
Alan Gomes <alan.gomes.ag28@gmail.com>
2026-04-11 22:00:40 -0300

    test: Adds tests for todo creation and user-todo relationship

--- tests/test_db.py
+++ tests/test_db.py
@@ -3,7 +3,7 @@ from dataclasses import asdict
 import pytest
 from sqlalchemy import select
 
-from fast_zero.models import User
+from fast_zero.models import Todo, User
 
 
 @pytest.mark.asyncio
@@ -24,6 +24,43 @@ async def test_create_user(session, mock_db_time):
             'username': 'Jhon',
             'password': '123456',
             'email': 'jhon@email.com',
+            'todos': [],
             'create_at': time,
             'updated_at': time,
         }
+
+
+@pytest.mark.asyncio
+async def test_create_todo(session, user):
+    todo = Todo(
+        title='Test Todo',
+        description='This is a test todo',
+        state='draft',
+        user_id=user.id,
+    )
+    session.add(todo)
+    await session.commit()
+    todo = await session.scalar(select(Todo))
+
+    assert asdict(todo) == {
+        'id': 1,
+        'title': 'Test Todo',
+        'description': 'This is a test todo',
+        'state': 'draft',
+        'user_id': user.id,
+    }
+
+
+@pytest.mark.asyncio
+async def test_user_todo_relationship(session, user):
+    todo = Todo(
+        title='Test Todo',
+        description='This is a test todo',
+        state='draft',
+        user_id=user.id,
+    )
+    session.add(todo)
+    await session.commit()
+    await session.refresh(user)
+
+    assert user.todos == [todo]
