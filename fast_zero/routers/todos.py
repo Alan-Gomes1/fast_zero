0418cdbdca3031d670a912e7af5485a9736bbd31
Alan Gomes <alan.gomes.ag28@gmail.com>
2026-04-14 18:20:59 -0300

    feat: Adds async endpoint for creating todos

--- fast_zero/routers/todos.py
+++ fast_zero/routers/todos.py
@@ -1,3 +1,33 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from fast_zero.database import get_session
+from fast_zero.models import Todo, User
+from fast_zero.schemas import TodoPublic, TodoSchema
+from fast_zero.security import get_current_user
+
+CurrentUser = Annotated[User, Depends(get_current_user)]
+Session = Annotated[AsyncSession, Depends(get_session)]
 
 router = APIRouter(prefix='/todos', tags=['todos'])
+
+
+@router.post('/', response_model=TodoPublic)
+async def create_todo(
+    todo: TodoSchema,
+    user: CurrentUser,
+    session: Session
+) -> Todo:
+    new_todo = Todo(
+        title=todo.title,
+        description=todo.description,
+        state=todo.state,
+        user_id=user.id
+    )
+    session.add(new_todo)
+    await session.commit()
+    await session.refresh(new_todo)
+
+    return new_todo
