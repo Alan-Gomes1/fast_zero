63c14df98420c39eba185fa5ee8a248f6adcafa0
Alan Gomes <alan.gomes.ag28@gmail.com>
2026-04-23 23:05:14 -0300

    test: Add async test to verify todo list returns correct count

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 import factory.fuzzy
+import pytest
 
 from fast_zero.models import Todo, TodoState
 
@@ -9,7 +10,7 @@ class TodoFactory(factory.Factory):
     class Meta:
         model = Todo
 
-    title = factory.Faker('text', max_nb_words=5)
+    title = factory.Faker('text', max_nb_chars=30)
     description = factory.Faker('text')
     state = factory.fuzzy.FuzzyChoice(TodoState)
     user_id = 1
@@ -32,3 +33,14 @@ def test_create_todo(client, token):
         'description': 'Test todo description',
         'state': 'draft',
     }
+
+
+@pytest.mark.asyncio
+async def test_list_todos_shold_return_5_todos(session, client, user, token):
+    expected_todos = 5
+    session.add_all(TodoFactory.create_batch(5, user_id=user.id))
+    await session.commit()
+
+    response = client.get('/todos/', headers={'Authorization': token})
+
+    assert len(response.json()['todos']) == expected_todos
